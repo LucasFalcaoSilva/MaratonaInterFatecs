c33dda20c5188a41c5503d6d41b8a281fa7d2e92
Luan-Victor <luvicantor@gmail.com>
2016-04-15 14:59:48 -0300

    Algumas modificações

--- app/src/main/java/br/maratonainterfatecs/activity/SponsorActivity.java
+++ app/src/main/java/br/maratonainterfatecs/activity/SponsorActivity.java
@@ -1,12 +1,17 @@
 package br.maratonainterfatecs.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import br.maratonainterfatecs.Domain.Sponsor;
 import br.maratonainterfatecs.R;
@@ -18,6 +23,7 @@ public class SponsorActivity extends AppCompatActivity {
     @Bind(R.id.toolbar)        Toolbar   mToolbar;
     @Bind(R.id.iv_logo)        ImageView ivLogo;
     @Bind(R.id.tv_description) TextView  tvDescription;
+    @Bind(R.id.btn_site)       Button    btnSite;
 
     Sponsor sponsor;
 
@@ -36,11 +42,21 @@ public class SponsorActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
 
+        btnSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(sponsor.getUrl()));
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
+
 
 }
